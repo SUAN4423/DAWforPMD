731298c37b9ec4d574ccb2546b6fa302bf05be98
3c1u <298748+3c1u@users.noreply.github.com>
2019-03-22 20:08:42 +0900

    ADD IChannel

---
+++ DAWforPMD/IChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAWforPMD.YM2608 {
+  interface IChannel {
+    void nextCycle(uint counter);
+    float getSample();
+  }
+}
--- DAWforPMD/YM2608.cs
+++ DAWforPMD/YM2608.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DAWforPMD
-{
-    class YM2608 : Register
-    {
-        public static float MasterClock = 8.0f; //メインクロック (MHz)
-        public static Byte FMDivision = 6;
-        public static Byte SSGDivision = 4;
+namespace DAWforPMD.YM2608 {
+  class YM2608 {
+    public static float MasterClock = 8.0f; //メインクロック (MHz)
+    public static Byte FMDivision = 6;
+    public static Byte SSGDivision = 4;
+
+    public List<IChannel> channels;
+
+    private const uint m_clock = 8000000;
+    private uint m_counter = 0;
+
+    private double m_correction = 0;
+
+    private const uint sample_rate = 44100;
+    private readonly uint m_cycles_per_sample = m_clock / sample_rate;
+    private readonly uint m_cycles_cor = m_clock % sample_rate;
+
+    private void generateSample(ref float[] buffer) {
+      for (var i = 0; i < buffer.Length; i++) {
+        var val = .0f;
+
+        // クロックのずれを補正する
+        var c = m_cycles_per_sample;
+        if (m_correction >= sample_rate) {
+          m_correction = m_correction - sample_rate;
+          c++;
+        }
+
+        foreach (var a in channels) {
+          // クロックのサイクルを回す
+          for (uint j = 0; j < c; j++)
+            a.nextCycle(m_counter + j);
+
+          // チャンネルの標本を取る
+          val = val + a.getSample();
+        }
+
+        buffer[i] = val;
+
+        // クロックを進める
+        m_counter += c;
+        m_correction += m_cycles_cor * c;
+      }
     }
+  }
 }
