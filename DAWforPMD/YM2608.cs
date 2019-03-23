7276ec0b68ecfeccf908bb5d7309a6f3a156c1b9
3c1u (Hikaru Terazono) <298748+3c1u@users.noreply.github.com>
2019-03-23 17:52:37 +0900

    SST.cs

--- DAWforPMD/YM2608.cs
+++ DAWforPMD/YM2608.cs
@@ -3,22 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace DAWforPMD.YM2608 {
-  class YM2608 {
+  public class YM2608 {
     public static float MasterClock = 8.0f; //メインクロック (MHz)
-    public static Byte FMDivision = 6;
-    public static Byte SSGDivision = 4;
+    public static Byte  FMDivision  = 6;
+    public static Byte  SSGDivision = 4;
 
     private const uint m_clock = 8000000;
 
     private double m_correction = 0;
 
-    private const uint sample_rate = 44100;
+    private const    uint sample_rate         = 44100;
     private readonly uint m_cycles_per_sample = m_clock / sample_rate;
-    private readonly uint m_cycles_cor = m_clock % sample_rate;
+    private readonly uint m_cycles_cor        = m_clock % sample_rate;
 
-    public IChannel[] channels;
-    private Stack<OPNAEvent>[] m_events_sorted;
-    private readonly uint m_ticks = 480;
+    private IChannel[] channels;
 
     private void generateSample(ref float[] buffer) {
       for (var i = 0; i < buffer.Length; i++) {
@@ -31,25 +29,14 @@ namespace DAWforPMD.YM2608 {
           c++;
         }
 
-        for (var i = 0; i < channels.Length; i++) {
-          var ch        = channels[i];
-          var nextEvent = m_events_sorted.Peek();
-
-          // クロックのサイクルを回す
-          for (uint j = 0; j < c; j++) {
-            
-            ch.nextCycle();
-          }
-
-          // チャンネルの標本を取る
-          val = val + ch.getSample();
-        }
-
-        buffer[i] = val;
-
         // クロックを進める
         m_correction += m_cycles_cor * c;
       }
     }
   }
+
+  private class TickGenerator {
+    
+  }
+
 }
