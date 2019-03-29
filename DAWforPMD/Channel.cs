184cb24761b4fdb79276fc7a4c8d75d0e08579bf
SUAN4423 <toyanonichan@yahoo.co.jp>
2019-03-29 13:25:24 +0900

    KeyOnCheck

--- DAWforPMD/Channel.cs
+++ DAWforPMD/Channel.cs
@@ -17,6 +17,7 @@ namespace DAWforPMD.YM2608
         private UInt16 FNumber = 0x0000;
         private Byte Block = 0x00;
         private Boolean[] KeyON = new Boolean[4] { false, false, false, false };
+        private Byte KeyONReg = 0x00;
 
         public Channel(int CHnum)
         {
@@ -34,8 +35,16 @@ namespace DAWforPMD.YM2608
             FNumber = Register.Read(this.ChannelNumber >= 3);
             Register.Select((Byte)(0xA4 + (this.ChannelNumber % 3)));
             Block = Register.Read(this.ChannelNumber >= 3);
-            FNumber += (UInt16)((Block & 0b00000111) << 8);
-            Block = (Byte)((Block & 0b00111000) >> 3);
+            FNumber += (UInt16)((UInt16)(Block & 0b00000111) << 8);
+            Block = (Byte)((Byte)(Block & 0b00111000) >> 3);
+
+            Register.Select(0x26);
+            KeyONReg = Register.Read(this.ChannelNumber >= 3);
+            if ((KeyONReg & 0b00000111) == Default.SelectChannel_KeyONOFF[this.ChannelNumber])
+            {
+                for (int i = 0; i < 4; i++) this.KeyON[i] = ((this.KeyONReg >> (4 + i)) & 0b01) == 0b1;
+            }
+
             Register.Select(0x29);
             if (this.ChannelNumber < 3 || Register.Read(false) >> 7 == 0b00000001 ? true : false)
             {
@@ -44,6 +53,7 @@ namespace DAWforPMD.YM2608
 
             //カウンタ計算
             this.counter += (UInt32)((1.0 / 9.0) * Math.Pow(2, Block + N - 15) * FNumber);
+            if (this.counter >= (UInt32)1024 << N) this.counter -= (UInt32)1024 << N;
             //throw new NotImplementedException();
         }
 
